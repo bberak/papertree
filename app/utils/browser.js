d24be652293b4c980083c31787499ad5953f6bd3
bberak <bberak@gmail.com>
2017-05-30 13:13:23 +1000

    Browser bug fix

--- app/utils/browser.js
+++ app/utils/browser.js
@@ -1,15 +1,33 @@
-import SafariView from 'react-native-safari-view'
-import { Linking } from 'react-native'
-import EStyleSheet from 'react-native-extended-stylesheet'
+import SafariView from "react-native-safari-view";
+import { Linking, StatusBar } from "react-native";
+import EStyleSheet from "react-native-extended-stylesheet";
 
 export default class Browser {
-	static openURL(url) {	
-	    SafariView.isAvailable()
-			.then(SafariView.show({
-				url: url
-			}))
+	static openURL(url) {
+		SafariView.isAvailable()
+			.then(() => {
+				const showSubscription = SafariView.addEventListener(
+					"onShow",
+					() => {
+						StatusBar.setBarStyle("default");
+						SafariView.removeEventListener(showSubscription);
+					}
+				);
+
+				const dismissSubscription = SafariView.addEventListener(
+					"onDismiss",
+					() => {
+						StatusBar.setBarStyle("light-content");
+						SafariView.removeEventListener(dismissSubscription);
+					}
+				);
+
+				return SafariView.show({
+					url: url
+				});
+			})
 			.catch(error => {
 				Linking.openURL(url);
-			});	
-  	}
+			});
+	}
 }
